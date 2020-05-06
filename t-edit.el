e49463fd5569c9655c4df5c881dbfef718fe5f1a
Tengfei Guo <guotengfei02@maoyan.com>
2020-05-06 16:11:39 +0800

    remove(feat): Remove revert buffer keys.

--- t-edit.el
+++ t-edit.el
@@ -24,7 +24,7 @@
 ;; display file path & and
 (map! "<f12>" (lambda() (interactive) (message (buffer-file-name))))
 ;; revert current buffer
-(map! "<f5>" 'revert-buffer)
+;; (map! "<f5>" 'revert-buffer)
 
 ;; key bindings
 (map! "M-i" 'terry3/insert-next-line)
@@ -33,19 +33,19 @@
 (map! "M-l" 'terry3/toggle-comment-current-line)
 
 ;; revert buffer no confirm
-(map! "<f5>"
-      (lambda (&optional force-reverting)
-        "Interactive call to revert-buffer. Ignoring the auto-save
- file and not requesting for confirmation. When the current buffer
- is modified, the command refuses to revert it, unless you specify
- the optional argument: force-reverting to true."
-        (interactive "P")
-        ;;(message "force-reverting value is %s" force-reverting)
-        (if (or force-reverting (not (buffer-modified-p)))
-            (progn
-              (revert-buffer :ignore-auto :noconfirm)
-              (message "revert file %s" (buffer-file-name)))
-          (error "The buffer has been modified"))))
+;; (map! "<f5>"
+;;       (lambda (&optional force-reverting)
+;;         "Interactive call to revert-buffer. Ignoring the auto-save
+;;  file and not requesting for confirmation. When the current buffer
+;;  is modified, the command refuses to revert it, unless you specify
+;;  the optional argument: force-reverting to true."
+;;         (interactive "P")
+;;         ;;(message "force-reverting value is %s" force-reverting)
+;;         (if (or force-reverting (not (buffer-modified-p)))
+;;             (progn
+;;               (revert-buffer :ignore-auto :noconfirm)
+;;               (message "revert file %s" (buffer-file-name)))
+;;           (error "The buffer has been modified"))))
 
 (provide 't-edit)
 ;;; t-edit ends here
